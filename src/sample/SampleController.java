6d8c28663f262bf524a44f6f84bf4c6588fdf709
ivy <iyoung3731@bths.edu>
2018-12-12 09:26:49 -0500

    12.12

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -5,25 +5,25 @@ import javafx.scene.control.Label;
 
 public class SampleController {
     public Label points;
-    private int sum = 0;
+    private Integer sum = 0;
 
     public void addOne(ActionEvent actionEvent) {
         sum++;
-        points.setText("Points: " + new Integer(sum).toString());
+        points.setText("Points: " + sum.toString());
     }
 
     public void addTen(ActionEvent actionEvent) {
         sum+=10;
-        points.setText("Points: " + new Integer(sum).toString());
+        points.setText("Points: " + sum.toString());
     }
 
     public void addOneHundred(ActionEvent actionEvent) {
         sum+=100;
-        points.setText("Points: " + new Integer(sum).toString());
+        points.setText("Points: " + sum.toString());
     }
 
     public void clear(ActionEvent actionEvent) {
         sum=0;
-        points.setText("Points: " + new Integer(sum).toString());
+        points.setText("Points: " + sum.toString());
     }
 }
